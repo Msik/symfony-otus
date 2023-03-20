08488784c6aa579c4b6dbb8bd483f0cfbef27f37
Maksim Khramov <maxim.khramov@mail.ru>
2023-03-20 18:42:18 +0300

    add user entity

---
+++ src/Entity/Trait/TimestampableTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity\Trait;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+trait TimestampableTrait
+{
+    #[ORM\Column(name: 'created_at', type: 'datetime', nullable: false)]
+    private DateTime $createdAt;
+
+    #[ORM\Column(name: 'updated_at', type: 'datetime', nullable: false)]
+    private DateTime $updatedAt;
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(): void
+    {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(): void
+    {
+        $this->updatedAt = new DateTime();
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\IdentiableTrait;
+use App\Entity\Trait\TimestampableTrait;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: '`user`')]
+#[ORM\Entity]
+class User
+{
+    use IdentiableTrait;
+    use TimestampableTrait;
+
+    #[ORM\Column(type: 'string', length: 32, nullable: false)]
+    private string $phone;
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'phone' => $this->phone,
+            'createdAt' => $this->createdAt->format('Y-m-d H:i:s'),
+            'updatedAt' => $this->updatedAt->format('Y-m-d H:i:s'),
+        ];
+    }
+}
