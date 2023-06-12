d5264e0bf0372dbf3ecab2ff6ea0a28e3b2bb0e5
Maksim Khramov <maxim.khramov@mail.ru>
2023-06-12 19:43:16 +0300

    fix test script

--- deploy/test.sh
+++ deploy/test.sh
@@ -6,7 +6,3 @@ sudo -u www-data sed -i -- "s|%DATABASE_NAME%|$5|g" .env
 sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction
 sudo -u www-data php bin/console ca:cl --no-interaction
 sudo -u www-data ./vendor/bin/simple-phpunit
-sudo -u www-data sed -i -- "s|%RABBITMQ_HOST%|$6|g" .env
-sudo -u www-data sed -i -- "s|%RABBITMQ_USER%|$7|g" .env
-sudo -u www-data sed -i -- "s|%RABBITMQ_PASSWORD%|$8|g" .env
-sudo service supervisor restart
